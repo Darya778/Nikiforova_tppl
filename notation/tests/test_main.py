d3f2ce8715c4133b68524908323f7c880f0294d5
Darya778 <112837727+Darya778@users.noreply.github.com>
2024-11-09 19:14:24 +0800

    Create test_main.py (tests for task "Перевод префиксной нотации в инфиксную")

---
+++ notation/tests/test_main.py
@@ -0,0 +1,25 @@
+import pytest
+from prefix_to_infix import PrefixToInfix
+
+
+@pytest.fixture(scope="function")
+def prefixtoinfix():
+    return PrefixToInfix()
+
+
+class TestPrefixToInfix:
+
+    def test_simple_expression(self, prefixtoinfix):
+        assert prefixtoinfix.convert("+ - 13 4 55") == "((13 - 4) + 55)"
+
+    def test_nested_expression(self, prefixtoinfix):
+        assert prefixtoinfix.convert("+ 2 * 2 - 2 1") == "(2 + (2 * (2 - 1)))"
+
+    def test_multiple_operators(self, prefixtoinfix):
+        assert prefixtoinfix.convert("+ + 10 20 30") == "((10 + 20) + 30)"
+
+    def test_subtraction_expression(self, prefixtoinfix):
+        assert prefixtoinfix.convert("- - 1 2") == "Not enough operands for operator"
+
+    def test_complex_expression(self, prefixtoinfix):
+        assert prefixtoinfix.convert("/ + 3 10 * + 2 3 - 3 5") == "((3 + 10) / ((2 + 3) * (3 - 5)))"
